e8adb6a682a9b608d584d8477db47afc0fa99357
Hugh McIntyre <hugh@hughtec.com>
2025-12-02 17:03:05 +1100

    build: add compiled frontend assets for recipe UI
    Part of #32

---
+++ backend/static/_app/immutable/chunks/BrdizEQO.js
@@ -0,0 +1,1 @@
+import{F as u,G as o,af as _,n as t,J as g,T as p,a5 as i,Q as l,I as n,R as m}from"./Clf_rkwX.js";function E(d,r){let a=null,f=t;var s;if(t){a=m;for(var e=g(document.head);e!==null&&(e.nodeType!==p||e.data!==d);)e=i(e);if(e===null)l(!1);else{var y=i(e);e.remove(),n(y)}}t||(s=document.head.appendChild(u()));try{o(()=>r(s),_)}finally{f&&(l(!0),n(a))}}export{E as h};

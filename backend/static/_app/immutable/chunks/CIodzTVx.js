91a2cf6bd3a8ad63b700103d0ecf20f926ddc368
Hugh McIntyre <hugh@hughtec.com>
2025-12-03 14:04:44 +1100

    feat: integrate modular card components into batch detail page

---
+++ backend/static/_app/immutable/chunks/CIodzTVx.js
@@ -0,0 +1,1 @@
+import{b as p,E as t}from"./DBUEpBwK.js";import{B as c}from"./Ce0eyuz5.js";function E(r,s,...a){var e=new c(r);p(()=>{const n=s()??null;e.ensure(n,n&&(o=>n(o,...a)))},t)}export{E as s};
---
+++ backend/static/_app/immutable/entry/start.CuLnrVB-.js
@@ -0,0 +1,1 @@
+import{l as o,a as r}from"../chunks/B1Yp9NyN.js";export{o as load_css,r as start};
